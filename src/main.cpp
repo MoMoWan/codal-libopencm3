297aeae693dbaff93ddd644c38fbe59dae09bc65
lupyuen <luppy@appkaki.com>
2018-11-09 20:01:37 +0800

    Fixed I2C interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 //  Main Application.  Based on https://github.com/LabAixBidouille-STM32/codal-stm32-iot-node/blob/master/samples/main.cpp
 #include "STM32BluePill.h"
+
+#ifdef PLATFORMIO  //  If building on PlatformIO...
+#include "i2cint.h"  //  Force I2C Interface to be included for PlatformIO build.
+#endif  //  PLATFORMIO
+
 using namespace codal;
 
 void Blink_main(codal::STM32BluePill& bluepill);
