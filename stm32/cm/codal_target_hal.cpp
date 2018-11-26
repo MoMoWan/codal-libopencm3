9228dee329109541268f3788dd391d1def97bc18
lupyuen <luppy@appkaki.com>
2018-11-26 22:48:52 +0800

    Added sleep and stop modes

--- stm32/cm/codal_target_hal.cpp
+++ stm32/cm/codal_target_hal.cpp
@@ -33,17 +33,17 @@ static void timer_tick() {
     if (tick_callback) { tick_callback(); }
 }
 
-void target_enable_debug() {
+void target_enable_debug(void) {
     //  Allow display of debug messages in development devices. NOTE: This will hang if no debugger is attached.
     enable_debug();   
 }
 
-void target_disable_debug() {
+void target_disable_debug(void) {
     //  Disable display of debug messages.  For use in production devices.
     disable_debug();  
 }
 
-void target_init() {
+void target_init(void) {
     //  Blue Pill specific initialisation...
     if (initialised) { return; }  //  Already initialised, skip.
     initialised = true;
@@ -90,7 +90,43 @@ void stm32bluepill_dmesg_flush() {
 #endif
 }
 
-void target_enter_deep_sleep_standby_mode() {
+#define SET_BIT(var, bit)   { var |= bit; }   //  Set the specified bit of var to 1, e.g. SET_BIT(SCB_SCR, SCB_SCR_SLEEPDEEP) sets bit SCB_SCR_SLEEPDEEP of SCB_SCR to 1.
+#define CLEAR_BIT(var, bit) { var &= ~bit; }  //  Set the specified bit of var to 0, e.g. CLEAR_BIT(SCB_SCR, SCB_SCR_SLEEPDEEP) sets bit SCB_SCR_SLEEPDEEP of SCB_SCR to 0.
+
+void target_enter_sleep_mode(void) {
+    //  To enter Sleep Now Mode: WFI (Wait for Interrupt) or WFE (Wait for Event) while:
+    //  – SLEEPDEEP = 0 and
+    //  – SLEEPONEXIT = 0 
+    //  Assume caller has set RTC Wakeup Alarm.
+    CLEAR_BIT(SCB_SCR, SCB_SCR_SLEEPDEEP);    //  Clear SLEEPDEEP bit of Cortex System Control Register.
+    CLEAR_BIT(SCB_SCR, SCB_SCR_SLEEPONEXIT);  //  Clear SLEEPONEXIT bit of Cortex System Control Register.
+    __asm("wfi");  //  Wait for interrupt from RTC Alarm.
+}
+
+void target_enter_deep_sleep_stop_mode(void) {
+    //  The Stop mode is based on the Cortex®-M3 deepsleep mode combined with peripheral
+    //  clock gating. The voltage regulator can be configured either in normal or low-power mode.
+    //  In Stop mode, all clocks in the 1.8 V domain are stopped, the PLL, the HSI and the HSE RC
+    //  oscillators are disabled. SRAM and register contents are preserved.
+    //  In the Stop mode, all I/O pins keep the same state as in the Run mode.
+    //  To enter Stop Mode: 
+    //  WFI (Wait for Interrupt) or WFE (Wait for Event) while:
+    //  – Set SLEEPDEEP bit in Cortex®-M3 System Control register
+    //  – Clear PDDS bit in Power Control register (PWR_CR)
+    //  – Select the voltage regulator mode by configuring LPDS bit in PWR_CR
+    //  Note: To enter Stop mode, all EXTI Line pending bits (in Pending register
+    //  (EXTI_PR)), all peripheral interrupt pending bits, and RTC Alarm flag must
+    //  be reset. Otherwise, the Stop mode entry procedure is ignored and
+    //  program execution continues. 
+    //  Assume caller has set RTC Wakeup Alarm.
+
+    pwr_set_stop_mode();   //  Clear PWR_CR_PDDS.
+    pwr_voltage_regulator_low_power_in_stop();  //  Switch voltage regulator to low power mode.
+    SET_BIT(SCB_SCR, SCB_SCR_SLEEPDEEP);        //  Set SLEEPDEEP bit of Cortex System Control Register.
+    __asm("wfi");  //  Wait for interrupt from RTC Alarm.
+}
+
+void target_enter_deep_sleep_standby_mode(void) {
     //  The Standby mode allows to achieve the lowest power consumption. It is based on the
     //  Cortex®-M3 deepsleep mode, with the voltage regulator disabled. The 1.8 V domain is
     //  consequently powered off. The PLL, the HSI oscillator and the HSE oscillator are also
@@ -101,14 +137,12 @@ void target_enter_deep_sleep_standby_mode() {
     //  – Set PDDS bit in Power Control register (PWR_CR)
     //  – Clear WUF bit in Power Control/Status register (PWR_CSR)
     //  – No interrupt (for WFI) or event (for WFI) is pending
-
-    //  TODO: Select the voltage regulator mode by setting LPDS bit in PWR register according to Regulator parameter value
-    //  MODIFY_REG(PWR->CR, PWR_CR_LPDS, Regulator);
+    //  Assume caller has set RTC Wakeup Alarm.
 
     pwr_set_standby_mode();   //  Set PWR_CR_PDDS.
     pwr_clear_wakeup_flag();  //  Clear WUF.    
-    SCB_SCR |= SCB_SCR_SLEEPDEEP;  //  Set SLEEPDEEP bit of Cortex System Control Register.
-    __asm("wfi");  //  Wait for interrupt.
+    SET_BIT(SCB_SCR, SCB_SCR_SLEEPDEEP);  //  Set SLEEPDEEP bit of Cortex System Control Register.
+    __asm("wfi");  //  Wait for interrupt from RTC Alarm.
 }
 
 void target_enable_irq() {
