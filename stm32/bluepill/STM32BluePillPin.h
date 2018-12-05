3274ee12966b889eaef3312af92a9808d1daa0ea
lupyuen <luppy@appkaki.com>
2018-12-05 15:47:28 +0800

    Fixed pin constructor

--- stm32/bluepill/STM32BluePillPin.h
+++ stm32/bluepill/STM32BluePillPin.h
@@ -5,6 +5,23 @@
 
 namespace codal {
     class STM32BluePillPin : public codal::_cm::Pin {
+    public:
+        //  Codal constructor
+        STM32BluePillPin(
+            int id,              //  e.g. DEVICE_ID_IO_P0 + CM_PIN_PC13
+            PinName       name,  //  e.g. CM_PIN_PC13
+            PinCapability capability  //  e.g. PIN_CAPABILITY_DIGITAL
+        ): codal::_cm::Pin(id, name, capability) {}
+
+        //  New constructor
+        STM32BluePillPin(
+            PinNumber   name,  //  e.g. CM_PIN_PC13
+            CmPinRCC    rcc,   //  e.g. RCC_GPIOC
+            CmPinPort   port,  //  e.g. GPIOC
+            CmPinNumber pin,   //  e.g. GPIO13
+            PinCapability capability  //  e.g. PIN_CAPABILITY_DIGITAL
+        ): codal::_cm::Pin(name, rcc, port, pin, capability) {}
+
 #ifdef NOTUSED        
     protected:
         void* pin;
