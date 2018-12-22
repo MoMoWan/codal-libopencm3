bdd08c55486b335483bf9a6ea012718e1193ba1e
lupyuen <luppy@appkaki.com>
2018-12-22 17:42:35 +0800

    Updating functions

--- lib/nano-float/src/functions.c
+++ lib/nano-float/src/functions.c
@@ -2,20 +2,26 @@
 #include <math.h>
 #include <qfplib.h>
 
+// CMakeFiles/STM32_BLUE_PILL.dir/pxtapp/base/core.cpp.o: In function `Math_::sqrt(pxt::TValueStruct*)':
+// /src/pxtapp/base/core.cpp:925: undefined reference to `sqrt'
+
+double sqrt(double x) { return qfp_fsqrt_fast(x); }
+
 // CMakeFiles/STM32_BLUE_PILL.dir/pxtapp/base/core.cpp.o: In function `Math_::log(pxt::TValueStruct*)':
 // /src/pxtapp/base/core.cpp:901: undefined reference to `log'
 
 double log(double x) { return qfp_fln(x); }
+double exp(double x) { return qfp_fexp(x); }
 
 // CMakeFiles/STM32_BLUE_PILL.dir/pxtapp/base/core.cpp.o: In function `Math_::log10(pxt::TValueStruct*)':
 // /src/pxtapp/base/core.cpp:904: undefined reference to `log10'
 
 double log10(double x) { return qfp_fln(x) / qfp_fln(10); }
 
-// CMakeFiles/STM32_BLUE_PILL.dir/pxtapp/base/core.cpp.o: In function `Math_::tan(pxt::TValueStruct*)':
-// /src/pxtapp/base/core.cpp:907: undefined reference to `tan'
+//  pow(b, x) = pow(e, log(b) * x) = exp(log(b) * x)
+//  e.g. pow(10, 3) = exp(log(10) * 3)
 
-double tan(double x) { return qfp_ftan(x); }
+double pow(double b, double x) { return qfp_fexp(qfp_fln(b) * x); }
 
 // CMakeFiles/STM32_BLUE_PILL.dir/pxtapp/base/core.cpp.o: In function `Math_::sin(pxt::TValueStruct*)':
 // /src/pxtapp/base/core.cpp:910: undefined reference to `sin'
@@ -26,6 +32,11 @@ double sin(double x) { return qfp_fsin(x); }
 // /src/pxtapp/base/core.cpp:913: undefined reference to `cos'
 
 double cos(double x) { return qfp_fcos(x); }
+
+// CMakeFiles/STM32_BLUE_PILL.dir/pxtapp/base/core.cpp.o: In function `Math_::tan(pxt::TValueStruct*)':
+// /src/pxtapp/base/core.cpp:907: undefined reference to `tan'
+
+double tan(double x) { return qfp_ftan(x); }
 
 // CMakeFiles/STM32_BLUE_PILL.dir/pxtapp/base/core.cpp.o: In function `Math_::atan(pxt::TValueStruct*)':
 // /src/pxtapp/base/core.cpp:916: undefined reference to `atan'
@@ -43,11 +54,6 @@ double atan2(double x, double y) { return qfp_fatan2(x, y); }
 
 ////  double acos(double x) { return (x); }
 
-// CMakeFiles/STM32_BLUE_PILL.dir/pxtapp/base/core.cpp.o: In function `Math_::sqrt(pxt::TValueStruct*)':
-// /src/pxtapp/base/core.cpp:925: undefined reference to `sqrt'
-
-double sqrt(double x) { return qfp_fsqrt_fast(x); }
-
 // CMakeFiles/STM32_BLUE_PILL.dir/pxtapp/base/core.cpp.o: In function `Math_::floor(pxt::TValueStruct*)':
 // /src/pxtapp/base/core.cpp:928: undefined reference to `floor'
 
@@ -63,13 +69,8 @@ double sqrt(double x) { return qfp_fsqrt_fast(x); }
 
 ////  double trunc(double x) { return (x); }
 
-//  pow(b, x) = pow(e, log(b) * x) = exp(log(b) * x)
-//  e.g. pow(10, 3) = exp(log(10) * 3)
-
-double pow(double b, double x) { return qfp_fexp(qfp_fln(b) * x); }
-
+//  TODO
 //double cosh(double x) { return (x); }
 //double sinh(double x) { return (x); }
-//double exp(double x) { return (x); }
 //double ldexp(double, int);
 //double fmod(double, double);
