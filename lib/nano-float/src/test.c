1de876b08a751d54b250a0974dabfa84b32acb05
lupyuen <luppy@appkaki.com>
2018-12-23 23:12:17 +0800

    Unit test

--- lib/nano-float/src/test.c
+++ lib/nano-float/src/test.c
@@ -2,6 +2,8 @@
 //  cd /mnt/c/qemu_stm32 ; arm-softmmu/qemu-system-arm -M stm32-f103c8 -semihosting -kernel /mnt/c/codal-libopencm3/.pioenvs/bluepill_f103c8/firmware.bin
 //  cd C:\qemu_stm32 && arm-none-eabi-gdb -ex="target remote localhost:1234" /codal-libopencm3/.pioenvs/bluepill_f103c8/firmware.elf
 #ifdef UNIT_TEST
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/rtc.h>
 #include <unity.h>
 #include <logger.h>
 #include <math.h>
@@ -16,7 +18,11 @@ void test_sqrt(void) {
 int test_nanofloat(void) {
     UNITY_BEGIN();
     RUN_TEST(test_sqrt);
-    return UNITY_END();
+    int fails = UNITY_END();
+    debug_flush();
+    //  Crash and exit.
+    rtc_awake_from_off(LSE);
+    return fails;
 }
 
 void unity_output_char(int ch) {
