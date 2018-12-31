dd7820e2fdbd81c31983b23220118c907bbbd474
lupyuen <luppy@appkaki.com>
2018-12-31 12:15:32 +0800

    Bootloader + timer ok

--- stm32/cm/CmTimer.cpp
+++ stm32/cm/CmTimer.cpp
@@ -14,17 +14,11 @@ namespace codal
         Timer *Timer::instance;
         static CODAL_TIMESTAMP trigger_period;
 
-        ////
+        //  cocoOS context and objects.
         static struct TimerContext {
         } context;
-        static struct TimerMsg {
-            Msg_t super;                          //  Required for all cocoOS messages.
-        } msg;
-        #define MSG_POOL_SIZE 2
-        static TimerMsg msg_pool[MSG_POOL_SIZE];  //  Pool of sensor data messages for the task message queue.
         static Sem_t timer_semaphore;
         static void timer_task(void);
-        ////
 
         Timer::Timer() : codal::Timer() {
             initialised = false;
@@ -51,19 +45,13 @@ namespace codal
 
         void tick_callback() {
             //  Will be called at every millisecond tick.  Needed to keep Codal scheduler running.
-            ////sem_ISR_signal(timer_semaphore); 
-
-            ////if (!Timer::instance) { return; }  //  No timer to trigger.
-            ////Timer::instance->trigger();
+            //  TODO: sem_ISR_signal(timer_semaphore); 
         }
 
         void alarm_callback() {
             //  Will be called when an alarm is triggered.  Needed to keep Codal scheduler running.
-            debug_print("ALM ");
+            debug_print("ALM ");  ////
             sem_ISR_signal(timer_semaphore); 
-
-            ////if (!Timer::instance) { return; }  //  No timer to trigger.
-            ////Timer::instance->trigger();
         }
 
         void Timer::init() {
@@ -72,18 +60,17 @@ namespace codal
             initialised = true;
             prev = millis();
 
-            ////
+            //  Create a semaphore to signal the Timer Task for a alarm or tick interrupt.
             timer_semaphore = sem_bin_create(0);  //  Binary Semaphore: Will wait until signalled.
-            //  uint8_t task_id = 
+
+            //  Create the cocoOS Timer Task.
             task_create(
                 timer_task,   //  Task will run this function.
-                &context,  //  task_get_data() will be set to the display object.
-                20,             //  Priority 20 = lower priority than UART task
-                (Msg_t *) msg_pool,  //  Pool to be used for storing the queue of messages.
-                MSG_POOL_SIZE,     //  Size of queue pool.
-                sizeof(TimerMsg));   //  Size of queue message.
-            ////
+                &context,     //  task_get_data() will be set to the context object.
+                20,           //  Priority 20
+                NULL, 0, 0);
 
+            //  Set the callbacks that will signal the Timer Task via Semaphore.
             target_set_tick_callback(tick_callback);
             target_set_alarm_callback(alarm_callback);
 
@@ -118,8 +105,7 @@ namespace codal
                 debug_println("triggerIn called before init");
                 return;
             }
-            //  debug_print("triggerIn "); debug_println((size_t) t); debug_flush(); ////
-            //  debug_printhex_unsigned(platform_get_alarm()); debug_print(" "); debug_flush(); ////
+            //  debug_print("triggerIn "); debug_println((size_t) t); debug_flush(); debug_printhex_unsigned(platform_get_alarm()); debug_print(" "); debug_flush(); ////
             platform_set_alarm(t);
             //  debug_printhex_unsigned(platform_get_alarm()); debug_print(" "); debug_flush(); ////
 #ifdef TODO
@@ -136,8 +122,6 @@ namespace codal
             target_enable_irq();
 #endif  //  TODO
         }
-
-        // extern "C" uint32_t uwTick;  //  Elapsed milliseconds.
 
         void Timer::syncRequest() {
             if (!initialised) {
@@ -160,19 +144,17 @@ namespace codal
             target_wait_us(us);
         }
 
-        ////
         static void timer_task(void) {
+            //  cocoOS task that runs forever waiting for the timer semaphore to be signalled by the alarm and tick interrupts.
             task_open();  //  Start of the task. Must be matched with task_close().  
             for (;;) {
-                // msg_receive(os_get_running_tid(), &msg);
-                sem_wait(timer_semaphore);
-                if (!Timer::instance) { continue; }  //  No timer to trigger.
+                sem_wait(timer_semaphore);           //  Wait for the semaphore to be signalled.
+                if (!Timer::instance) { continue; }  //  No timer to trigger, quit.
                 debug_print(" >> "); ////
-                Timer::instance->trigger();
+                Timer::instance->trigger();          //  Trigger the CODAL Scheduler.
             }
             task_close();  //  End of the task. Should not come here.
         }
-        ////
 
     }  //  namespace _cm
 
