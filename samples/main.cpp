df3fa4ea71faf1dd30d2086df6988d066238cde7
lupyuen <luppy@appkaki.com>
2019-01-03 11:53:54 +0800

    Merged sample and src main.cpp

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,15 +1,7 @@
 //  Main Application for CODAL build.  Based on https://github.com/LabAixBidouille-STM32/codal-stm32-iot-node/blob/master/samples/main.cpp
-//  TODO: Sync with src/main.cpp
 //  #define DISABLE_DEBUG ////
 #include <logger.h>
 #include <STM32BluePill.h>
-
-#ifdef PLATFORMIO    //  If building on PlatformIO...
-#include "i2cint.h"  //  Force I2C Interface to be included for PlatformIO build.
-#include "spiint.h"  //  Force SPI Interface to be included for PlatformIO build.
-#include "adcint.h"  //  Force ADC Interface to be included for PlatformIO build.
-#include "unity.h"   //  Force Unity unit test to be included for PlatformIO build.
-#endif  //  PLATFORMIO
 
 using namespace codal;
 
@@ -63,5 +55,5 @@ static void debug_dump(codal::STM32BluePill& bluepill, const char *msg) {
     // debug_print(", us "); debug_print((size_t) bluepill.timer.getTimeUs()); 
     debug_print(", alm "); debug_print((size_t) platform_alarm_count()); 
     // debug_print(", tick "); debug_print((size_t) platform_tick_count()); 
-    debug_println("");
+    debug_println(""); debug_flush(); ////
 }
