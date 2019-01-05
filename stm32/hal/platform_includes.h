10056ed8fcabcf4617e6519c7a0865fd9cd35035
lupyuen <luppy@appkaki.com>
2019-01-05 13:16:30 +0800

    Clean up

--- stm32/hal/platform_includes.h
+++ stm32/hal/platform_includes.h
@@ -8,42 +8,37 @@
 #include <stdlib.h>  //  For malloc()
 
 #ifdef PLATFORMIO  //  Define target symbols only for PlatformIO build, not Codal build.
-////  TODO: Sync with target.json. Based on https://github.com/mmoskal/codal-generic-f103re/blob/master/target.json
+//  TODO: Sync with https://github.com/lupyuen/codal-libopencm3/blob/master/target.json
 
-//  Sync with linker def: ld/stm32f103x8.ld
-//  TODO: Should use _data and _stack but codal-core build fails because they are undefined.
-//  #define DEVICE_SRAM_BASE &_data   //  Based on STM32F103C8, SRAM=0x20000000 to 0x20005000 (20KB)
-//  #define DEVICE_SRAM_END  &_stack  //  Based on STM32F103C8, SRAM=0x20000000 to 0x20005000 (20KB)
-#define DEVICE_SRAM_BASE 0x20000000   //  Based on STM32F103C8, SRAM=0x20000000 to 0x20005000 (20KB)
-#define DEVICE_SRAM_END  0x20005000   //  Based on STM32F103C8, SRAM=0x20000000 to 0x20005000 (20KB)
-#define DEVICE_STACK_BASE DEVICE_SRAM_END
-#define DEVICE_STACK_SIZE 2048
-
-#define LIGHTWEIGHT_EVENTS 1  //  Avoid long division in codal::scheduler_tick()
-#define PROCESSOR_WORD_TYPE uint32_t
-#define CODAL_TIMESTAMP     uint32_t  //  TODO: Previously uint64_t. To reduce code size.
-#define USB_MAX_PKT_SIZE 64
-#define DEVICE_USB_ENDPOINTS 8
-#define USB_DEFAULT_VID 0x1209  //  TODO: Sync with https://github.com/lupyuen/bluepill-bootloader/blob/master/src/usb_conf.h
-#define USB_DEFAULT_PID 0xDB42  //  TODO: Sync with https://github.com/lupyuen/bluepill-bootloader/blob/master/src/usb_conf.h
-#define USB_EP_FLAG_NO_AUTO_ZLP 0x01
-#define TARGET_DEBUG_CLASS NOT_IMPLEMENTED
-#define DEVICE_HEAP_ALLOCATOR 1
-#define DEVICE_TAG 0
-#define SCHEDULER_TICK_PERIOD_US 4000
-#define EVENT_LISTENER_DEFAULT_FLAGS MESSAGE_BUS_LISTENER_QUEUE_IF_BUSY
-#define MESSAGE_BUS_LISTENER_MAX_QUEUE_DEPTH 10
-#define USE_ACCEL_LSB 0
-#define DEVICE_DEFAULT_SERIAL_MODE SYNC_SLEEP
-#define DEVICE_COMPONENT_COUNT 60
-#define DEVICE_DEFAULT_PULLMODE PullMode::None
-#define DEVICE_PANIC_HEAP_FULL 1
-#define DEVICE_DMESG 1
-#define DEVICE_DMESG_BUFFER_SIZE 256  //  Was 1024
-#define CODAL_DEBUG CODAL_DEBUG_DISABLED
-#define DEVICE_USB 0  //  TODO: Previously 1.  Disable flashing by USB.  Requires UF2 to be included for build.
-#define BOOTLOADER_START_ADDR 0x08000000  //  TODO: Sync with https://github.com/lupyuen/bluepill-bootloader/blob/master/src/stm32f103/stm32f103x8.ld
-#define BOOTLOADER_END_ADDR   0x08006800  //  TODO: Sync with https://github.com/lupyuen/bluepill-bootloader/blob/master/src/stm32f103/stm32f103x8.ld
+#define LIGHTWEIGHT_EVENTS          1           //  Avoid long division in codal::scheduler_tick()
+#define PROCESSOR_WORD_TYPE         uint32_t
+#define CODAL_TIMESTAMP             uint32_t    //  TODO: Previously uint64_t. To reduce code size.
+#define USB_MAX_PKT_SIZE            64
+#define DEVICE_USB_ENDPOINTS        8
+#define USB_DEFAULT_VID             0x1209      //  TODO: Sync with https://github.com/lupyuen/codal-libopencm3/blob/master/stm32/bootloader/usb_conf.h
+#define USB_DEFAULT_PID             0xDB42      //  TODO: Sync with https://github.com/lupyuen/codal-libopencm3/blob/master/stm32/bootloader/usb_conf.h
+#define USB_EP_FLAG_NO_AUTO_ZLP     0x01
+#define DEVICE_SRAM_BASE            0x20000000  //  Based on STM32F103C8, SRAM=0x20000000 to 0x20005000 (20KB)
+#define DEVICE_SRAM_END             0x20005000
+#define DEVICE_STACK_BASE           DEVICE_SRAM_END
+#define DEVICE_STACK_SIZE           2048
+#define TARGET_DEBUG_CLASS          NOT_IMPLEMENTED
+#define DEVICE_HEAP_ALLOCATOR       1
+#define DEVICE_TAG                  0
+#define SCHEDULER_TICK_PERIOD_US                4000
+#define EVENT_LISTENER_DEFAULT_FLAGS            MESSAGE_BUS_LISTENER_QUEUE_IF_BUSY
+#define MESSAGE_BUS_LISTENER_MAX_QUEUE_DEPTH    10
+#define USE_ACCEL_LSB               0
+#define DEVICE_DEFAULT_SERIAL_MODE  SYNC_SLEEP
+#define DEVICE_COMPONENT_COUNT      60
+#define DEVICE_DEFAULT_PULLMODE     PullMode::None
+#define DEVICE_PANIC_HEAP_FULL      1
+#define DEVICE_DMESG                1
+#define DEVICE_DMESG_BUFFER_SIZE    256           //  Was 1024
+#define CODAL_DEBUG                 CODAL_DEBUG_DISABLED
+#define DEVICE_USB                  0             //  TODO: Previously 1.  Disable flashing by USB.  Requires UF2 to be included for build.
+#define BOOTLOADER_START_ADDR       0x08000000    //  TODO: Sync with https://github.com/lupyuen/codal-libopencm3/blob/master/ld/stm32f103x8.ld
+#define BOOTLOADER_END_ADDR         0x08006800    //  TODO: Sync with https://github.com/lupyuen/codal-libopencm3/blob/master/ld/stm32f103x8.ld
 #endif  //  PLATFORMIO
 
 #ifdef __cplusplus
